621c0df01ebefbc13224dd38f6223f47c2cda1f7
nimra <nimrasha120@gmail.com>
2026-06-18 23:29:46 +0500

    login api updated

--- src/componenets/Login.jsx
+++ src/componenets/Login.jsx
@@ -17,7 +17,7 @@ function Login() {
     setError("");
 
     try {
-      const response = await fetch("http://localhost:5000/login", {
+      const response = await fetch("https://nimra-backend.onrender.com/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
